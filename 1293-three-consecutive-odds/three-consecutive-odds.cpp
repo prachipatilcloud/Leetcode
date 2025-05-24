ba2ccfb17e7cfb920e86effa0cb3b47b00493a82
prachipatilcloud <misspatil.04@gmail.com>
2025-05-24 13:52:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (83.18%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        int count = 0;
+
+        for(int i : arr){
+            if(i % 2 != 0){
+                count++;
+                if(count == 3){
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+};
