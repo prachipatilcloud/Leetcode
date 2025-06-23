70bfff0e6d60d5e87a6c16d6f75395b3ce0de151
prachipatilcloud <misspatil.04@gmail.com>
2025-06-23 23:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (91.80%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double power = pow(x,n);
-        return power;
+        long long power = n;
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+        
+        double result = 1.0;
+        while (power > 0) {
+            if (power % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            power /= 2;
+        }
+        return result;
     }
 };
