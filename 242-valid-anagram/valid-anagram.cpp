0a5e01a19d9d693073a9e573ff10286a1bd06cac
prachipatilcloud <misspatil.04@gmail.com>
2025-08-12 15:02:48 +0530

    Time: 4 ms (39.15%) | Memory: 9.8 MB (26.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>count1,count2;
-
-        for(char ch : s){
-            count1[ch]++;
+        unordered_map<char,int> count1,count2;
+        for(char c: s){
+            count1[c]++;
         }
-        for(char ch : t){
-            count2[ch]++;
+        for(char c: t){
+            count2[c]++;
         }
-
         if(count1 == count2){
             return true;
         }
