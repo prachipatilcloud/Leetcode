fdf2dff702cbec8956b96c23135b965849f20060
prachipatilcloud <misspatil.04@gmail.com>
2025-09-13 20:12:02 +0530

    Time: 668 ms (13.04%) | Memory: 0B (100.00%) - LeetSync

--- 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select a.teacher_id, count(distinct a.subject_id) as cnt
-from Teacher a
+select teacher_id, count(distinct subject_id) as cnt
+from Teacher 
 group by teacher_id;
