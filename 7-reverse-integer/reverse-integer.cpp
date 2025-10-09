993afda6b3e31917b34d4540f4daba6fa35506af
prachipatilcloud <misspatil.04@gmail.com>
2025-10-10 01:11:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (82.40%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
-        int rev =0;
-
-        while (x !=0) {
-            int digit = x%10;
-
-            if(rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7)) return 0;
-            if(rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8)) return 0;
-
-            rev= rev*10 +digit;
-            x/= 10;
+        int i = 0 , ans = 0;
+        while(x != 0){
+            int digit = x % 10;
+            if((ans > INT_MAX/10) || (ans < INT_MIN/10)){
+                return 0;
+            }
+            ans = (ans * 10) + digit;
+            x = x / 10;
         }
-
-        return rev;
+        return ans;
     }
 };
