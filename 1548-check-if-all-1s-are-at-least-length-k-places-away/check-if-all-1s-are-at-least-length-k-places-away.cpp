d555cec57d3865ffea8471e90e15675a77afa489
prachipatilcloud <misspatil.04@gmail.com>
2025-11-22 00:32:19 +0530

    Time: 0 ms (100.00%) | Memory: 61.4 MB (72.18%) - LeetSync

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int last_one_index = -1;
+        for(int i =0; i<nums.size(); i++){
+            if(nums[i] == 1){
+                if(last_one_index != -1 && (i - last_one_index -1)< k){
+                    return false;
+                }
+                  last_one_index = i;
+            }
+        }
+        return true;
+    }
+};
